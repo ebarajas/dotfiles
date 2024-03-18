97e9df2952d8c2a331240519ec0ce79af3751f13
Ernesto Barajas <ebarajas@figma.com>
2024-03-18 08:37:37 -0700

    only export figma env vars if file exists

--- .config/zsh/config.d/01_figma.zsh
+++ .config/zsh/config.d/01_figma.zsh
@@ -1,3 +1,7 @@
+if [[ ! -a ~/.figma ]]; then
+    return
+fi
+
 export PATH="${HOMEBREW_PREFIX}/opt/openssl/bin:$PATH"
 export PKG_CONFIG_PATH="/opt/homebrew/opt/zlib/lib/pkgconfig:/usr/local/opt/zlib/lib/pkgconfig:$PKG_CONFIG_PATH"
 export PKG_CONFIG_PATH="/opt/homebrew/opt/openssl@3/lib/pkgconfig:/usr/local/opt/openssl@3/lib/pkgconfig:$PKG_CONFIG_PATH"
