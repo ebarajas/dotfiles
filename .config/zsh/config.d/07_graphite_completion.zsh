55ac02519e1221c00e221724b5cfef47d15ef476
Ernesto Barajas <ebarajas@figma.com>
2024-04-23 09:51:02 -0700

    add zsh completion for graphite

---
+++ .config/zsh/config.d/07_graphite_completion.zsh
@@ -0,0 +1,16 @@
+#compdef gt
+###-begin-gt-completions-###
+#
+# yargs command completion script
+#
+_gt_yargs_completions()
+{
+  local reply
+  local si=$IFS
+  IFS=$'
+' reply=($(COMP_CWORD="$((CURRENT-1))" COMP_LINE="$BUFFER" COMP_POINT="$CURSOR" gt --get-yargs-completions "${words[@]}"))
+  IFS=$si
+  _describe 'values' reply
+}
+compdef _gt_yargs_completions gt
+###-end-gt-completions-###
