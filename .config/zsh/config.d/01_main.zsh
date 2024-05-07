562c7efbfd7552858190571f5c79af567a18a8e4
Ernesto Barajas <ebarajas@figma.com>
2024-05-07 16:59:37 -0700

    bump up history file size

--- .config/zsh/config.d/01_main.zsh
+++ .config/zsh/config.d/01_main.zsh
@@ -4,11 +4,14 @@ setopt no_beep
 setopt auto_cd	
 setopt prompt_subst
 
-setopt append_history		# Have each shell session append to the history file rather than replace it
-setopt extended_history		# Save the beginning timestamp and elapsed time with each command
-setopt inc_append_history	# Add lines to history incrementally rather than waiting for the shell to exit
-setopt hist_ignore_dups		# Don't add lines to history if they're dupes of the previous line 
+setopt append_history		    # Have each shell session append to the history file rather than replace it
+setopt extended_history		    # Save the beginning timestamp and elapsed time with each command
+setopt inc_append_history	    # Add lines to history incrementally rather than waiting for the shell to exit
+setopt hist_ignore_dups		    # Don't add lines to history if they're dupes of the previous line 
 setopt hist_expire_dups_first	# Remove duplicate lines first when trimming history 
+
+export HISTSIZE=1000000000
+export SAVEHIST=$HISTSIZE
 
 unsetopt complete_aliases	# Unset this so completions work with aliases
 
